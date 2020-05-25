e30703b7152befacca63d348664f3163195e1a04
rohitcoder <rohittourister@gmail.com>
2020-05-25 20:40:28 +0530

    Commiting first version

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BBB-AWS-S3
+
 This script is written in python, and it keeps migrating your all recorded videos/sessions from Big Blue Button server to your desired s3 bucket. 
---
+++ bbb-s3.py
@@ -0,0 +1,77 @@
+import os, glob, shutil, boto3
+from botocore.exceptions import NoCredentialsError
+
+## Configuration Part
+ACCESS_KEY = ''
+SECRET_KEY = ''
+bucket_name = ''
+DELETE_SERVER_FILES = True ## Set False (F should be capital) if you don't want to delete files from bbb-server
+
+def upload_to_aws(local_file, bucket, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+
+    try:
+        s3.upload_file(local_file, bucket, s3_file) 
+        return True
+    except FileNotFoundError: 
+        return False
+    except NoCredentialsError: 
+        return False
+
+def getListOfFiles(dirName): 
+    listOfFile = os.listdir(dirName)
+    allFiles = list() 
+    for entry in listOfFile: 
+        fullPath = os.path.join(dirName, entry) 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles     
+
+def getListOfDirs(path):
+    return [os.path.basename(x) for x in filter(
+        os.path.isdir, glob.glob(os.path.join(path, '*')))]
+
+def remove(path):
+    """ param <path> could either be relative or absolute. """
+    if os.path.isfile(path) or os.path.islink(path):
+        os.remove(path)  # remove the file
+    elif os.path.isdir(path):
+        shutil.rmtree(path)  # remove dir and all contains
+    else:
+        raise ValueError("file {} is not a file or dir.".format(path))
+
+def main():
+    
+    dirName = os.getcwd(); 
+    listOfFiles = getListOfFiles(dirName) 
+    listOfDirs = getListOfDirs(dirName)
+    for elem in listOfFiles:
+        listOfFiles = list()
+    for (dirpath, dirnames, filenames) in os.walk(dirName):
+        listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+
+    for elem in listOfFiles: 
+        relative_path = elem.replace(dirName,'')[1:]
+        if(relative_path!='bbb-s3.py'):
+            print("\nUploading "+relative_path)    
+            uploaded = upload_to_aws(elem, bucket_name, relative_path)
+            if(uploaded and DELETE_SERVER_FILES):
+                remove(elem)
+            else:
+                print("\nNot able to delete files from your bbb-server, check file if its uploaded")
+
+    file_length = len(listOfFiles)
+    if(file_length==1):
+        for folder in listOfDirs:
+            remove(folder)
+
+        
+        
+        
+        
+if __name__ == '__main__':
+    main()
